7b8a2306e0718fce043490bd435c7ef78b5ff794
Per Christian B. Viken <perchr@northblue.org>
2026-04-11 12:24:41 +0200

    feat(lib, annotations): Allow ignoring properties

--- TypeContractor.Annotations/TypeContractorIgnoreAttribute.cs
+++ TypeContractor.Annotations/TypeContractorIgnoreAttribute.cs
@@ -3,11 +3,11 @@ using System;
 namespace TypeContractor.Annotations
 {
 	/// <summary>
-	/// Tells TypeContractor to ignore the given controller when generating
-	/// automatic API clients. For example a controller that serves static
-	/// assets for the HTML templates.
+	/// Tells TypeContractor to ignore the target when generating TypeScript.
+	/// For example a controller that serves static assets for HTML templates.
+	/// Can also be used for properties that don't need to be exposed.
 	/// </summary>
-	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
 	public sealed class TypeContractorIgnoreAttribute : Attribute
 	{
 	}
