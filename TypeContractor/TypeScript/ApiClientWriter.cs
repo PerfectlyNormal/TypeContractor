3a30b4bfd726e0fa252804222b073a08dcb40296
Per Christian B. Viken <perchr@northblue.org>
2026-06-14 10:20:12 +0200

    fix(lib): `DELETE` endpoints should not have bodies
    RFC 9110[1] specifies "A client SHOULD NOT generate content in a
    DELETE request", so don't do it here either. We don't know what
    intermediaries might be along the request chain, so don't assume it
    works. The previous commit passes `IsDelete` to the template, allowing
    us to generate clients using the Aurelia template, since that library
    allows bodies in `DELETE` requests.
    
    [1]: https://www.rfc-editor.org/rfc/rfc9110.html#section-9.3.5-6

--- TypeContractor/TypeScript/ApiClientWriter.cs
+++ TypeContractor/TypeScript/ApiClientWriter.cs
@@ -105,7 +105,7 @@ public partial class ApiClientWriter(string outputPath, string? relativeRoot)
 				}
 			}
 
-			var requiresBody = endpoint.Method is EndpointMethod.POST or EndpointMethod.PUT or EndpointMethod.PATCH or EndpointMethod.DELETE;
+			var requiresBody = endpoint.Method is EndpointMethod.POST or EndpointMethod.PUT or EndpointMethod.PATCH;
 			var body = endpoint.Parameters.FirstOrDefault(p => p.FromBody || (!p.FromRoute && !p.FromQuery));
 
 			var returnUnparsedResponse = endpoint.UnwrappedReturnType is null && endpoint.ReturnType is null;
